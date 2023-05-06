158beffd6bd87689d75fbb4a667cf50a2f2cf97d
Andrés Mata <andres.mata@olympian.org>
2023-05-06 11:10:17 +0200

    Hemos añadido el proyecto de Android Studio

---
+++ app/src/main/java/com/example/roaddoor/FormActivity.java
@@ -0,0 +1,15 @@
+package com.example.roaddoor;
+
+import android.app.Activity;
+
+public class FormActivity extends Activity {
+
+    public FormActivity() {
+        super();
+    }
+
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
